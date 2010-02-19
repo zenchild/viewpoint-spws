885cffbd6e5b34f303db3063e8884338be9afe26
Dan Wanek <dwanek@woden.asgard.net>
2010-02-19 09:09:33 -0600

    added sp_field

---
+++ lib/model/sp_field.rb
@@ -0,0 +1,8 @@
+module Viewpoint
+  module Sharepoint
+    class SPField
+      def initialize()
+      end
+    end
+  end
+end
--- lib/spws.rb
+++ lib/spws.rb
@@ -1,3 +1,7 @@
 require 'config_loader'
-require 'model/sp_list'
+# Load the backend SOAP infrastructure.  Today this is Handsoap.
 require 'soap/soap_provider'
+# Load the model classes
+require 'model/sp_list'
+require 'model/sp_list_item'
+require 'model/sp_field'
