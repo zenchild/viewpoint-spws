3918a1ba871f5ae5b0bb18b3886d12317147a7e5
Dan Wanek <dan.wanek@gmail.com>
2011-12-28 13:29:53 -0600

    add initial specs

---
+++ spec/lists_spec.rb
@@ -0,0 +1,9 @@
+$: << File.dirname(__FILE__)
+require 'spec_helper'
+
+describe 'Test the Sharepoint List web service functionality' do
+
+  it 'should retrieve the Lists from a given Sharepoint site'
+  it 'should retrieve the Items from a given List'
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+$: << File.dirname(__FILE__) + '/../lib/'
+require 'date'
+
+module SpecHelper
+  def self.specdir
+    File.dirname(__FILE__)
+  end
+end
