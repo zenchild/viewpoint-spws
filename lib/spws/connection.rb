7c455510bdadc69104dc7bb1b48a25c24dda48a4
Dan Wanek <dan.wanek@gmail.com>
2011-12-29 11:55:29 -0600

    Changes to Connection class

--- lib/spws/connection.rb
+++ lib/spws/connection.rb
@@ -5,6 +5,7 @@ class Viewpoint::SPWS::Connection
   #   web service part.
   #   @example https://<site>/personal/myname
   def initialize(site_base)
+    @log = Logging.logger[self.class.name.to_s.to_sym]
     @httpcli = HTTPClient.new
     site_base = site_base.end_with?('/') ? site_base : site_base << '/'
     @site_base = URI.parse(site_base)
@@ -16,6 +17,10 @@ class Viewpoint::SPWS::Connection
 
   def lists_ws
     Lists.new(self)
+  end
+
+  def usergroup_ws
+    UserGroup.new(self)
   end
 
   # Authenticate to the web service
@@ -35,7 +40,7 @@ class Viewpoint::SPWS::Connection
   # @return [String] If the request is successful (200) it returns the body of
   #   the response.
   def post(websvc, xmldoc)
-    headers = {'Content-Type' => 'text/xml; charset=utf-8'}
+    headers = {'Content-Type' => 'application/soap+xml; charset=utf-8'}
     url = (@site_base + websvc).to_s
     check_response( @httpcli.post(url, xmldoc, headers) )
   end
@@ -47,9 +52,26 @@ class Viewpoint::SPWS::Connection
     case resp.status
     when 200
       resp.body
+    when 500
+      if resp.headers['Content-Type'].include?('xml')
+        err_string, err_code = parse_soap_error(resp.body)
+        raise "SOAP Error: Message: #{err_string}  Code: #{err_code}"
+      else
+        raise "Internal Server Error. Message: #{resp.body}"
+      end
     else
       raise "HTTP Error Code: #{resp.status}, Msg: #{resp.body}"
     end
+  end
+
+  # @param [String] xml to parse the errors from.
+  def parse_soap_error(xml)
+    ndoc = Nokogiri::XML(xml)
+    ns = ndoc.collect_namespaces
+    err_string  = ndoc.xpath("//xmlns:errorstring",ns).text
+    err_code    = ndoc.xpath("//xmlns:errorcode",ns).text
+    @log.debug "Internal SOAP error. Message: #{err_string}, Code: #{err_code}"
+    [err_string, err_code]
   end
 
 end
