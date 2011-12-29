0ff6ed5f74e4ef057495e816d51ab88b1442e9fb
Dan Wanek <dan.wanek@gmail.com>
2011-12-29 12:42:50 -0600

    List specs

--- spec/lists_spec.rb
+++ spec/lists_spec.rb
@@ -2,8 +2,18 @@ $: << File.dirname(__FILE__)
 require 'spec_helper'
 
 describe 'Test the Sharepoint List web service functionality' do
+  before(:all) do
+    con = Viewpoint::SPWS::Connection.new(@conf[:site])
+    con.set_auth(@conf[:user],@conf[:pass])
+    @lws = con.lists_ws
+  end
 
-  it 'should retrieve the Lists from a given Sharepoint site'
+  it 'should retrieve the Lists from a given Sharepoint site' do
+    lists = @lws.get_list_collection(true)
+    lists.should be_an_instance_of(Array)
+    lists.first.should be_an_instance_of(Viewpoint::SPWS::List)
+  end
+
   it 'should retrieve the Items from a given List'
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,22 @@
 $: << File.dirname(__FILE__) + '/../lib/'
+require 'yaml'
 require 'date'
+require 'viewpoint_spws'
+
+# To run these tests put configuration into a file called site_info.yaml
+# --- 
+# :site: sp_site_base
+# :user: user
+# :pass: pass
 
 module SpecHelper
   def self.specdir
     File.dirname(__FILE__)
+  end
+end
+
+RSpec.configure do |config|
+  config.before(:all) do
+    @conf = YAML.load(File.open("#{SpecHelper.specdir}/site_info.yaml",'r'))
   end
 end
