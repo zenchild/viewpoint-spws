79a16e845027f7aa2ac2ae003c103c8fc8d8ce8d
Dan Wanek <dan.wanek@gmail.com>
2011-12-30 14:54:30 -0600

    almost full support for GetListItems

--- lib/spws/websvc/lists.rb
+++ lib/spws/websvc/lists.rb
@@ -77,16 +77,63 @@ class Viewpoint::SPWS::Lists
   # Get List Items based on certain parameters
   # @see http://msdn.microsoft.com/en-us/library/lists.lists.getlistitems(v=office.12).aspx
   # @param [String] list title or the GUID for the list
-  # @param [Hash] parms
-  #   :view_name GUID for the view surrounded by curly braces 
-  def get_list_items(list, parms = {})
+  # @param [Hash] opts
+  # @option opts [String] :view_name ('') GUID for the view surrounded by curly braces 
+  #   If nothing is passed it used the default of the View
+  # @option opts [String] :row_limit ('') A String representing the number of rows to return.
+  # @option opts [Boolean] :recursive (true) If true look in subfolders as well as root
+  # @option opts [Boolean] :date_in_utc (true) If true return dates in UTC
+  # @option opts [String]  :folder ('') 
+  #   Filter document library items for items in the specified folder
+  # @yield [builder] Yields a Builder object that can be used to build a CAML Query. See the
+  #   example below on how to use it.
+  # @yieldparam [Nokogiro::XML::Builder] builder The builder object used to create the Query
+  # @example The following example shows how to prepare a CAML Query with a block. It
+  #   filters for all objects of ObjectType '0' = Files
+  #   items = listws.get_list_items('Shared Documents',:recursive => true) do |b|
+  #     b.Query {
+  #       b.Where {
+  #         b.Eq {
+  #           b.FieldRef(:Name => 'FSObjType')
+  #           b.Value(0, :Type => 'Integer')
+  #         }
+  #       }
+  #     }
+  #   end
+  def get_list_items(list, opts = {})
+    # Set Default values
+    opts[:recursive] = true unless opts.has_key?(:recursive)
+    opts[:view_name] = '' unless opts.has_key?(:view_name)
+    opts[:row_limit] = '' unless opts.has_key?(:row_limit)
+    opts[:date_in_utc] = true unless opts.has_key?(:date_in_utc)
+    opts[:folder] = '' unless opts.has_key?(:folder)
+
     soapmsg = build_soap_envelope do |type, builder|
       if(type == :header)
       else
         builder.GetListItems {
           builder.parent.default_namespace = @default_ns
           builder.listName(list)
-          builder.viewName(parms[:view_name])
+          builder.viewName(opts[:view_name])
+          builder.rowLimit(opts[:row_limit])
+
+          if block_given?
+            builder.query {
+              builder.parent.default_namespace = ''
+              yield builder
+            }
+          end
+
+          builder.queryOptions {
+            builder.QueryOptions {
+              builder.parent.default_namespace = ''
+              builder.Folder(opts[:folder])
+              builder.ViewAttributes(:Scope => 'Recursive') if opts[:recursive]
+              builder.DateInUtc('True') if opts[:date_in_utc]
+            }
+          }
+          # @todo Is this worth supporting???
+          #builder.webID(parms[:web_id])
         }
       end
     end
